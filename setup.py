4d9b8f1e5eabecdf7bda0e112101fef3bc58d2b0
Tim Burke <timburke@alumni.rice.edu>
2017-10-30 17:32:42 -0700

    Add envelope calculation function

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         "future>=0.16.0",
         "numpy>=1.13.1",
         "pandas>=0.20.3",
+        "scipy>=1.0.0",
         "matplotlib>=2.0.2",
         "typedargs>=0.10.0",
         "iotile_cloud>=0.7.0",
