91a3b58c0f0165141945ede14b26ddd1095dca60
Tim Burke <timburke@alumni.rice.edu>
2018-01-26 13:55:31 -0800

    Remove old pytest plugin from setup.py

--- iotile_analytics_core/setup.py
+++ iotile_analytics_core/setup.py
@@ -16,7 +16,7 @@ setup(
         "pandas>=0.20.3",
         "scipy>=1.0.0",
         "typedargs>=0.10.0",
-        "iotile_cloud>=0.7.4",
+        "iotile_cloud>=0.8.0",
         "tqdm>=4.19.4"
     ],
     description="A data science bridge for iotile.cloud",
