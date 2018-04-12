f468c025ebc45214c5fd16ba305682b5e2fc1326
Tim Burke <timburke@alumni.rice.edu>
2018-04-12 12:52:46 -0700

    Improve LogLog viewer and add AnalyticsApplication concept

--- iotile_analytics_interactive/iotile_analytics/interactive/__init__.py
+++ iotile_analytics_interactive/iotile_analytics/interactive/__init__.py
@@ -2,5 +2,6 @@ from .viewer_timeseries import TimeseriesViewer
 from .viewer_loglog import LogLogViewer
 from .viewer_linear import LinearViewer
 from .viewer_base import BaseViewer
+from .app import AnalyticsObject, AnalyticsApplication, show
 
-__all__ = ['TimeseriesViewer', 'LogLogViewer', 'LinearViewer', 'BaseViewer']
+__all__ = ['TimeseriesViewer', 'LogLogViewer', 'LinearViewer', 'BaseViewer', 'AnalyticsApplication', 'AnalyticsObject', 'show']
---
+++ iotile_analytics_interactive/iotile_analytics/interactive/viewer_semilog_x.py
@@ -0,0 +1,8 @@
+from .viewer_base import BaseViewer
+
+
+class SemilogXViewer(BaseViewer):
+    """A data series viewer with a logarithmic x axis.
+
+    All functionality in BaseViewer is present and there are a few
+    convenience functions
