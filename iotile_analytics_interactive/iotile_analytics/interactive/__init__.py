db4eb63cfa8b8d022f5fca9aae104b304a3c1026
Tim Burke <timburke@alumni.rice.edu>
2018-04-12 17:55:22 -0700

    Remove old imports

--- iotile_analytics_interactive/iotile_analytics/interactive/__init__.py
+++ iotile_analytics_interactive/iotile_analytics/interactive/__init__.py
@@ -1,6 +1,4 @@
-from .viewer_timeseries import TimeseriesViewer
 from .viewer_loglog import LogLogViewer
-from .viewer_linear import LinearViewer
 from .viewer_base import BaseViewer
 from .app import AnalyticsObject, AnalyticsApplication, show
 
