e01efe3d8641d508764e7392ad250c3287d3d032
Tim Burke <timburke@alumni.rice.edu>
2018-04-20 14:22:49 -0700

    Install scipy, numpy

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ if on_rtd:
         def __getattr__(cls, name):
             return MagicMock()
 
-    MOCK_MODULES = ['pandas', 'tables']
+    MOCK_MODULES = ['tables']
     sys.modules.update((mod_name, _Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
